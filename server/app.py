069b3a0a0695b0080d703fed47381f12193e294c
Rostomi <36961172+rosjerry@users.noreply.github.com>
2026-02-20 18:49:27 +0400

    Add app.py

---
+++ server/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_socketio import SocketIO
+app=Flask(__name__)
+app.config['SECRET_KEY']='secret'
+socketio=SocketIO(app,cors_allowed_origins='*')
+
+@app.route('/health')
+def health():
+    return 'ok'
+
+if __name__=='__main__':
+    socketio.run(app)
